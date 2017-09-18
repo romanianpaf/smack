ac68dfb8219b38d4671e129ffa30bb4e85565948
Alexandru-Florentin Popescu <alexandru.popescu@m.ro>
2017-09-18 20:04:00 +0300

    login vc

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Alexandru Popescu on 18.09.2017.
+//  Copyright © 2017 Alexandru Popescu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Alexandru Popescu on 18.09.2017.
+//  Copyright © 2017 Alexandru Popescu. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
